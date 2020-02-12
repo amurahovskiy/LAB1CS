371314494bbbeeb795dcb2e3ea3033cb2d48b701
Vlad <amurahovskiy@gmail.com>
2020-02-12 12:50:32 +0200

    add all

---
+++ MyBase64.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DronTropy
+{
+    public static class MyBase64
+    {
+        private static List<int> listBits;
+        private static Dictionary<int, char> keySymbol = new Dictionary<int, char>();
+        private static string element = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        static MyBase64()
+        {
+            int key = 0;
+            foreach (var el in element)
+            {
+                keySymbol.Add(key, el);
+                key++;
+            }
+        }
+        private static int TranFor10System(List<int> byts)
+        {
+
+            int decimalNumber = 0;
+            for (int i = 0; i < byts.Count; i++)
+            {
+                if (byts[byts.Count - i - 1] == 0) continue;
+                decimalNumber += (int)Math.Pow(2, i);
+            }
+            return decimalNumber;
+        }
+        private static List<int> FillBits(byte byt)
+        {
+            var result = new List<int>();
+            var bits = new Stack<int>();
+            for (var index = 0; index < 8; index++)
+            {
+                bits.Push(byt % 2);
+                byt /= 2;
+            }
+
+            return bits.ToList();
+        }
+        public static string GetBase64(string pathFile)
+        {
+            listBits = new List<int>();
+            var listByteFile = File.ReadAllBytes(pathFile).ToList();
+            foreach (var byt in listByteFile)
+            {
+                listBits.AddRange(FillBits(byt));
+            }
+            listBits.AddRange(listBits.Count % 6 != 0 ? new int[(listBits.Count / 6 + 1) * 6 - listBits.Count].ToList() : new List<int>(0));
+
+            List<int> resultNumber = new List<int>();
+            var temp = new List<int>();
+            int count = 0;
+            string s = "";
+            foreach (var el in listBits)
+            {
+                temp.Add(el);
+                count++;
+                if (count == 6)
+                {
+                    resultNumber.Add(TranFor10System(temp));
+                    temp = new List<int>();
+                    count = 0;
+                }
+            }
+
+            string resultBase64 = null;
+            foreach (var el in resultNumber)
+            {
+                resultBase64 += keySymbol[el];
+            }
+
+            var append = listByteFile.Count % 3 == 1 ? "==" : listByteFile.Count % 3 == 2 ? "=" : "";
+            resultBase64 += append;
+            return resultBase64;
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DronTropy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayPath = new string[3] { @"D:\Text\OneText", @"D:\Text\TwoText", @"D:\Text\ThreeText" };
+            FileInfo file = null;
+            List<string> result = new List<string>();
+            string[] arrayBASE64 = new string[3];
+            var listEnt = new List<List<Entropiy>>();
+            List<Entropiy> oneText = new List<Entropiy>();
+            List<Entropiy> twoText = new List<Entropiy>();
+            List<Entropiy> threeText = new List<Entropiy>();
+            listEnt.Add(oneText);
+            listEnt.Add(twoText);
+            listEnt.Add(threeText);
+            for(var index=0; index < 3; index++)
+            {
+                listEnt[index].Add(new Entropiy(arrayPath[index]+".txt", true));
+                listEnt[index].Add(new Entropiy(MyBase64.GetBase64(arrayPath[index] + ".txt")));
+                listEnt[index].Add(new Entropiy(MyBase64.GetBase64(arrayPath[index] + ".7z")));
+            }
+            int count = 0;
+            foreach(var elem in listEnt)
+            {
+                foreach(var ent in elem)
+                {
+                    result.Add(ent.Path);
+                    result.AddRange(ent.GetProbality());
+                    result.Add("Ентропія:" + ent.Entropiya.ToString());
+                    result.Add("Кількість інформації байтах :" + ent.AmountInformation + "  в битах:" + ent.AmountInformation / 8);
+                    result.Add("Дліна тексту" + ent.Text.Length);                   
+                    count++;
+                }
+                count = 0;
+                result.Add("-------------------");
+            }
+
+            foreach (var el in arrayPath)
+            {
+                
+                file = new FileInfo(el + ".txt");
+                result.Add(el + " " + file.Length/8);
+            }
+            foreach (var el in arrayPath)
+            {
+                file = new FileInfo(el + ".7z");
+                result.Add(el + " " + file.Length / 8);
+            }
+            result.Add("OneText.txt: " + MyBase64.GetBase64(@"D:\Text\OneText.txt"));
+            result.Add("TwoText.txt: " + MyBase64.GetBase64(@"D:\Text\TwoText.txt"));
+            result.Add("ThreeText.txt: " + MyBase64.GetBase64(@"D:\Text\ThreeText.txt"));
+            result.Add("-------");
+            result.Add("OneText.7z: " + MyBase64.GetBase64(@"D:\Text\TwoText.7z"));
+            result.Add("TwoText.7z: " + MyBase64.GetBase64(@"D:\Text\TwoText.7z"));
+            result.Add("Three.7z: " + MyBase64.GetBase64(@"D:\Text\ThreeText.7z"));
+            using (StreamWriter sw = new StreamWriter(@"C:\log.txt", true))
+            {
+                foreach (var el in result)
+                {
+                    sw.WriteLine(el);
+                }
+            }
+            Console.WriteLine(MyBase64.GetBase64(@"D:\Text\TwoText.txt"));
+     
+           
+            Console.ReadKey();
+        }
+    }
+    public
+    class Entropiy
+    {
+        public Dictionary<char, double> charProbability = null;
+        public string Text { get; set; }
+
+        public double Entropiya
+        {
+            get;
+            private set;
+        }
+        public List<string> GetProbality()
+        {
+            var result = new List<string>();
+            foreach (char key in charProbability.Keys.ToArray())
+            {
+             
+                result.Add("Symbol " + key + " " + charProbability[key].ToString());
+            }
+            return result;
+        }
+        public double AmountInformation
+        {
+            get
+            {
+                return this.Text.Length * this.Entropiya;
+            }
+        }
+
+        public string Path { get; set; }
+        public Entropiy(string path, bool isFile = false)
+        {
+         
+            if (isFile)
+            {
+                this.Path = path;
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Text = sr.ReadToEnd();
+                }
+            }
+            else
+            {
+                this.Path = "Base64 or 7z";
+                this.Text = path;
+            }
+
+            this.charProbability = new Dictionary<char, double>();
+
+            foreach (var el in Text)//get count every char in text
+            {
+                if (!charProbability.ContainsKey(el))
+                {
+                    charProbability.Add(el, 1);
+                }
+                else
+                {
+                    charProbability[el] = charProbability[el] + 1;
+                }
+            }
+
+            int textLen = Text.Length;
+            foreach (char key in charProbability.Keys.ToArray())
+            {
+                charProbability[key] = charProbability[key] / textLen;//get probability as (count this char in text) / (text len)
+            }
+
+            foreach (double val in charProbability.Values)
+            {
+                Entropiya += val * Math.Log(1 /val, 2);
+            }        
+        }
+    }
+}
